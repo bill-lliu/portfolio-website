5b7bfcca32a042d6e81416188056d267f9e261f4
Bill Liu <billliusiyi@gmail.com>
2023-10-05 19:34:15 -0400

    add react 3 fiber

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-// import * as THREE from 'three'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
