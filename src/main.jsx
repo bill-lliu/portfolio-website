a4ba3e257c58c8323f6cdc7a80c12782d947de1d
Bill Liu <billliusiyi@gmail.com>
2023-10-14 18:04:14 -0400

    add default styles

--- src/main.jsx
+++ src/main.jsx
@@ -8,10 +8,21 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <MantineProvider defaultColorScheme="auto">
-      <ScreenProvider>
+    <ScreenProvider>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        defaultColorScheme="light"
+        theme={{
+          headings: { fontFamily: "Cabin Sketch, sans-serif" },
+          // headings: { fontFamily: "Patrick Hand, cursive" },
+          fontFamily: "Balsamiq Sans, sans-serif",
+          primaryColor: "violet",
+          defaultRadius: "xl",
+        }}
+      >
         <App />
-      </ScreenProvider>
-    </MantineProvider>
+      </MantineProvider>
+    </ScreenProvider>
   </React.StrictMode>
 );
