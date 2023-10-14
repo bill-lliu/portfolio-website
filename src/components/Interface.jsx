79d085a2d441488a04479224b8f425cb5ab64bed
Bill Liu <billliusiyi@gmail.com>
2023-10-13 20:07:34 -0400

    implement user interface layer

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -1,7 +1,27 @@
+import { Affix, Button, Stack } from "@mantine/core";
+import { ScreenViews, useScreenContext } from "../contexts/ScreenContext";
+
 const Interface = () => {
+  const { screen, setScreen, cameraMode, setCameraMode } = useScreenContext();
+
   return (
     <>
-      <h1 className="text-red-500">Hello world!</h1>
+      <Affix position={{ top: 20, right: 20 }}>
+        <Stack>
+          {Object.keys(ScreenViews).map((view) => (
+            <Button
+              key={view}
+              onClick={() => setScreen(view)}
+              variant={screen === view ? "gradient" : "light"}
+              gradient={{ from: "violet", to: "grape" }}
+              radius="lg"
+              color="violet"
+            >
+              <h4>{view}</h4>
+            </Button>
+          ))}
+        </Stack>
+      </Affix>
     </>
   );
 };
