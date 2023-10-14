7b1c9ee706d609ce9d55a0659e9e68e5a7e0a154
Bill Liu <billliusiyi@gmail.com>
2023-10-14 18:04:54 -0400

    add screen positions and targets

--- src/contexts/ScreenContext.jsx
+++ src/contexts/ScreenContext.jsx
@@ -1,10 +1,15 @@
 import { createContext, useContext, useState } from "react";
 
 export const ScreenViews = {
-  home: "home",
-  about: "about",
-  projects: "projects",
-  contact: "contact",
+  Home: { position: [6, -4, 20], target: [0, 0, 0] },
+  About: { position: [1.2, -1.4, 7.6], target: [0, -2.3, 2] },
+  // Timeline: "Timeline",
+  // Experience: "Experience",
+  Projects: { position: [-4, 4, 2], target: [-4, 4, -10] },
+  // Resume: "Resume",
+  // Services: "Services",
+  Contact: { position: [2.8, 3.5, -2], target: [2.8, 4.5, -10] },
+  // Socials: "Socials",
 };
 
 export const CameraModes = {
@@ -19,7 +24,7 @@ export const useScreenContext = () => {
 };
 
 export const ScreenProvider = ({ children }) => {
-  const [screen, setScreen] = useState(ScreenViews.home);
+  const [screen, setScreen] = useState(ScreenViews.Home);
   const [cameraMode, setCameraMode] = useState(CameraModes.free);
 
   return (
