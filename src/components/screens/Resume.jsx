8699bd39176be9bf948f1fc4148290e2c16d2ed4
Bill Liu <billliusiyi@gmail.com>
2023-10-17 08:28:54 -0400

    update resume

--- src/components/screens/Resume.jsx
+++ src/components/screens/Resume.jsx
@@ -25,7 +25,33 @@ const Resume = () => {
         <Affix className="screen">
           <Stack gap={0}>
             <Title className="title">Resume</Title>
-            <Text className="content">loren ipsum</Text>
+            <Text className="content">
+              I plan on building an interactive Resume here in the near future,
+              but until then, I hope a lamer{" "}
+              <a
+                href="https://drive.google.com/file/d/143hGgYdllWrADoPRR9ain-MD1eEiIoke/view?usp=sharing"
+                target="_blank"
+                rel="noreferrer"
+              >
+                PDF version
+              </a>{" "}
+              will suffice.
+            </Text>
+            <br />
+            <Text className="content">
+              {" "}
+              <i>
+                &#40;In the offchance the link doesn&apos;t work&#41;:
+                <br />
+                <a
+                  href="https://drive.google.com/file/d/143hGgYdllWrADoPRR9ain-MD1eEiIoke/view?usp=sharing"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  https://drive.google.com/file/d/143hGgYdllWrADoPRR9ain-MD1eEiIoke/view?usp=sharing
+                </a>
+              </i>
+            </Text>
           </Stack>
         </Affix>
       ) : null}
