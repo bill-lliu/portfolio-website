dfd18bef7187b4ac39ea078a447351dc7145f1f7
Bill Liu <billliusiyi@gmail.com>
2023-10-17 02:02:34 -0400

    add loader

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,3 +1,4 @@
+import { Loader } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import CameraController from "../contexts/CameraController";
@@ -12,20 +13,27 @@ const Experience = () => {
     setCurrentCameraMode,
   } = useScreenContext();
 
-  const Loading = () => {
-    return <p>loading...</p>;
-  };
   // Canvas used to set up three.js's scene, camera, and renderer
   return (
-    <Canvas shadows camera={{ position: [6, 0, 20], fov: 30, near: 1.2 }}>
-      <CameraController />
-      <Suspense fallback={null}>
-        <Scene position-x={0} position-y={-4} position-z={2} />
-        <fog attach="fog" args={["#D390F2", 15, 75]} />
-        {/* <ambientLight intensity={0.5} /> */}
-        {/* <Environment preset="dawn" background /> */}
-      </Suspense>
-    </Canvas>
+    <>
+      <Canvas shadows camera={{ position: [6, 0, 20], fov: 30, near: 1.2 }}>
+        <CameraController />
+        <Suspense fallback={null}>
+          <Scene position-x={0} position-y={-4} position-z={2} />
+          <fog attach="fog" args={["#D390F2", 15, 75]} />
+          {/* <ambientLight intensity={0.5} /> */}
+          {/* <Environment preset="dawn" background /> */}
+        </Suspense>
+      </Canvas>
+      <Loader
+        containerStyles={{}} // ...container - Flex layout styles
+        innerStyles={{}} // ...inner - Inner container styles
+        barStyles={{}} // ...bar - Loading-bar styles
+        dataStyles={{}} // ...data - Text styles
+        dataInterpolation={(p) => `Loading ${p.toFixed(2)}%`} // Text
+        initialState={(active) => active} // Initial black out state
+      />
+    </>
   );
 };
 
