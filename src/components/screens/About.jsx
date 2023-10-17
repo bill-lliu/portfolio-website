f6d0cc4ebcd34c0a6d73c7ca330c0611c1335d11
Bill Liu <billliusiyi@gmail.com>
2023-10-17 07:58:19 -0400

    update about page

--- src/components/screens/About.jsx
+++ src/components/screens/About.jsx
@@ -25,7 +25,51 @@ const About = () => {
         <Affix className="screen">
           <Stack gap={0}>
             <Title className="title">About Myself</Title>
-            <Text className="content">loren ipsum</Text>
+            <div className="content">
+              <Text>
+                Hi! My name is Bill, and welcome to the latest iteration of my
+                website. I&apos;ve always wanted to try a 3D project on the web,
+                so this refresh seemed like the perfect opportunity.
+                &#128736;&#65039;
+              </Text>
+              <br />
+              <Text>
+                This website is still a work in progress, but my most recent
+                commits can be found at this project&apos;s{" "}
+                <a
+                  href="https://github.com/bill-lliu/portfolio-website"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  GitHub repo.
+                </a>{" "}
+                &#128161;
+              </Text>
+              <br />
+              <Text>
+                I&apos;m currently a 4B Comp Eng student at the University of
+                Waterloo, with 7 swe internships under my belt. I have
+                experience from producing clean, test-driven code at large
+                multi-billion dollar tech corporations; to prototyping,
+                iterating, and pivoting products/features at seed-stage startups
+                of &lt;10 people. &#129489;&#8205;&#128187;
+              </Text>
+              <br />
+              <Text>
+                I&apos;m currently seeking 2024 new grad or internship
+                opportunities at companies with cool products and people, so if
+                you think I&apos;d be a good fit, I&apos;d love to connect!
+                &#128522;
+              </Text>
+              <br />
+              <Text>
+                Thanks for visiting! Feel free to shoot me a text in the chat at
+                the bottom right &#128071;
+              </Text>
+              <Text>
+                <i>&#40;I&apos;ll get notifs on my phone&#41;</i>
+              </Text>
+            </div>
           </Stack>
         </Affix>
       ) : null}
